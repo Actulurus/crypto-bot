b28862e6c91dac27823d043c63d2a3060f1d1a1c
actulurus <actulurus@gmail.com>
2023-11-18 19:20:45 +0100

    test mode & output log

--- main.py
+++ main.py
@@ -2,16 +2,45 @@ import ccxt
 import pandas as pd
 import json
 import time
+import sys
 
 from colorama import Fore, Back, Style
+
+try:
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+except FileNotFoundError:
+    print(Fore.YELLOW + "config.json not found - created one for you. Please fill it out and restart the program.")
+
+    with open('config.json', 'w') as config_file:
+        config = {
+            "symbol": "BTC/USDT",
+            "timeframe": "1m",
+            "short_window": 10,
+            "long_window": 30,
+            "api_key": "Your api key here",
+            "api_secret": "Your api secret here"
+        }
+
+        json.dump(config, config_file, indent=4)
+    
+    exit()
 
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
+arg1 = sys.argv[1] if len(sys.argv) > 1 else None
+arg2 = sys.argv[2] if len(sys.argv) > 2 else None
+arg3 = sys.argv[3] if len(sys.argv) > 3 else None
+arg4 = sys.argv[4] if len(sys.argv) > 4 else None
+
+IS_TEST_MODE = (arg3 or "") == "True"
+TEST_DURATION = int(arg4 or "0")
+
 symbol = config['symbol']
 timeframe = config['timeframe']
-short_window = config['short_window']
-long_window = config['long_window']
+short_window = int(arg1 or config['short_window'])
+long_window = int(arg2 or config['long_window'])
 
 api_key = config['api_key']
 api_secret = config['api_secret']
@@ -22,6 +51,8 @@ exchange = ccxt.cryptocom({
     'enableRateLimit': True,
 })
 
+runtime_start = time.time()
+
 last_action = "None"
 last_balance = 0
 
@@ -31,7 +62,10 @@ balance_crypto = 0
 initial_balance = balance_usd
 
 print(Fore.YELLOW + "------------------------")
-print("Running...")
+if IS_TEST_MODE:
+    print("Running in TEST mode...")
+else:
+    print("Running in LIVE mode...")
 print("------------------------")
 
 def order(action):
@@ -94,6 +128,18 @@ def execute_orders(df):
     order(action)
 
 while True:
+    if IS_TEST_MODE:
+        if time.time() - runtime_start >= TEST_DURATION:
+            print(Fore.YELLOW + "Test duration reached - exiting...")
+
+            # Write a log file
+            with open('TestOutput/log_' + str(short_window) + '_' + str(long_window) + '_' + str(runtime_start) + '.txt', 'w') as log_file:
+                log_file.write("Short window: " + str(short_window) + "\n")
+                log_file.write("Long window: " + str(long_window) + "\n")
+                log_file.write("Profit: " + str(get_total_balance() - initial_balance) + " USD (" + str((get_total_balance() - initial_balance) / initial_balance * 100) + "%)")
+
+            exit()
+    
     try:
         df = fetch_ohlcv(symbol, timeframe)
         df = calculate_moving_averages(df, short_window, long_window)
--- test.py
+++ test.py
@@ -1,92 +1,31 @@
-candle = [ # green doji
-    0,
-    1,
-    3,
-    0,
-    2
-]
+# Use this script to test the bot with different arguments (short & long window).
 
-candle2 = [ # red doji
-    0,
-    3,
-    5,
-    0,
-    2
-]
+import multiprocessing
+import subprocess
 
-candle3 = [ # green candle, no lower wick
-    0,
-    1,
-    5,
-    1,
-    3
-]
+TEST_DURATION = 15 #60 * 60 * 8 # In seconds
 
-candle4 = [ # red candle, no upper wick
-    0,
-    3,
-    3,
-    0,
-    1
-]
+def run_script(script_path, args):
+    command = ["python", script_path] + args
+    subprocess.run(command)
 
-def get_candle_body(candle):
-    return candle[4] - candle[1]
-def get_candle_size(candle):
-    return abs(get_candle_body(candle))
-def get_candle_direction(candle):
-    if get_candle_body(candle) > 0:
-        return 1
-    elif get_candle_body(candle) < 0:
-        return -1
-    else:
-        return 0
+if __name__ == "__main__":
+    script_to_run = "main.py"
 
-def get_lower_wick(candle):
-    if get_candle_direction(candle) > 0:
-        return candle[1] - candle[3]
-    elif get_candle_direction(candle) < 0:
-        return candle[4] - candle[3]
+    script_arguments_list = [
+        ['3', '6'],
+        ['3', '12'],
+        ['5', '10'],
+        ['5', '20'],
+        ['10', '20'],
+        ['10', '40'],
+        ['15', '30'],
+        ['15', '60'],
+    ]
 
-def get_upper_wick(candle):
-    if get_candle_direction(candle) > 0:
-        return candle[2] - candle[4]
-    elif get_candle_direction(candle) < 0:
-        return candle[2] - candle[1]
+    for args in script_arguments_list:
+        args.append("True")
+        args.append(str(TEST_DURATION))
 
-def get_length_of_opposite_wick(candle):
-    if get_candle_body(candle) > 0:
-        return get_lower_wick(candle)
-    elif get_candle_body(candle) < 0:
-        return get_upper_wick(candle)
-    else:
-        return 0
-def is_opposite_wick_short_enough(candle):
-    return get_length_of_opposite_wick(candle) < 0.05
-
-def is_doji(candle):
-    avg_wick = (get_lower_wick(candle) - get_upper_wick(candle)) / 2
-    are_wicks_long_enough = get_lower_wick(candle) > get_candle_size(candle) * 0.9 and get_upper_wick(candle) > get_candle_size(candle) * 0.9
-    is_body_small = get_candle_size(candle) < avg_wick * 1.1
-
-    return are_wicks_long_enough and is_body_small 
-
-def is_green_no_lower_wick(candle):
-    return candle[1] < candle[4] and abs(candle[1] - candle[3]) < 0.05
-
-def is_red_no_upper_wick(candle):
-    return candle[1] > candle[4] and abs(candle[2] - candle[1]) < 0.05
-
-def is_doji(candle):
-    w1, w2 = 0, 0
-    if candle[1] > candle[4]:
-        w1 = candle[2] - candle[1]
-        w2 = candle[4] - candle[3]
-    elif candle[1] < candle[4]:
-        w1 = candle[4] - candle[3]
-        w2 = candle[2] - candle[1]
-    print(w1, w2)
-    # checks whether body is smaller than the average wick size and whether the difference between the wicks is smaller than the size of the body
-    return abs(candle[1] - candle[4]) < (w1 + w2) / 2 and abs(w1 - w2) < abs(candle[1] - candle[4])
-
-print(is_doji(candle2))
+    with multiprocessing.Pool() as pool:
+        pool.starmap(run_script, [(script_to_run, args) for args in script_arguments_list])
