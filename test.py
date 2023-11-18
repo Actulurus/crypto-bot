c212383b82167071c4e895fdadd0c194da5280f3
actulurus <actulurus@gmail.com>
2023-11-18 20:09:30 +0100

    clear output on startup, test time is now an arg

--- test.py
+++ test.py
@@ -2,16 +2,28 @@
 
 import multiprocessing
 import subprocess
+import sys
+import time
+import os
 
 from colorama import init, Fore, Back, Style
 
-TEST_DURATION = 15 #60 * 60 * 8 # In seconds
+TEST_DURATION = int(sys.argv[1]) if len(sys.argv) > 1 else 60 * 30
 
 def run_script(script_path, args):
     command = ["python", script_path] + args
     subprocess.run(command)
 
 if __name__ == "__main__":
+    print(Fore.YELLOW + "Starting test mode with duration of " + str(TEST_DURATION) + " seconds.")
+    print("Started on " + str(time.time()) + ".")
+    print(Fore.WHITE + "---------------------------------")
+
+    # clear output folder (all files except .gitkeep)
+    for file in os.listdir("TestOutput"):
+        if file != ".gitkeep":
+            os.remove("TestOutput/" + file)
+
     script_to_run = "main.py"
 
     script_arguments_list = [
